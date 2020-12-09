0fb2f20fa3d375c49811904c312e6d6a4e1175fb
Mathieu <degracia.mathieu@gmail.com>
2020-12-09 19:22:11 +0100

    Add project

---
+++ forge.php
@@ -0,0 +1,23 @@
+<?php
+
+$fp = fopen('src/isEven.php', 'w');
+
+$string = '<?php' . PHP_EOL . PHP_EOL;
+$string .= 'namespace DeGraciaMathieu\IsEven;' . PHP_EOL . PHP_EOL;
+$string .= 'function isEven(int $number): bool' . PHP_EOL;
+$string .= '{' . PHP_EOL;
+
+for ($i=0; $i <= 100; $i++) { 
+
+	$bool = ($i % 2 == 0) ? 'true' : 'false';
+
+	$string .= "    if (\$number == $i) {";
+	$string .= " return $bool; ";
+	$string .= '}' . PHP_EOL;
+}
+
+$string .= '}' . PHP_EOL;
+
+fwrite($fp, $string);
+
+fclose($fp);
---
+++ tests/IsEvenTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DeGraciaMathieu\IsEven\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class IsEvenTest extends TestCase
+{
+    /** 
+     * @test
+     * @dataProvider dataProvider
+     */
+    public function check(int $number, bool $expected)
+    {
+        $isEven = \DeGraciaMathieu\IsEven\isEven($number);
+
+        $this->assertEquals($expected, $isEven);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [2, true],
+            [3, false],
+            // [1008, true],
+            // [10009, false],
+        ];
+    }
+}
